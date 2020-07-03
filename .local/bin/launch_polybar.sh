63d1ae7057edec16fbfde396358834b8f7414e17
Maxim Mikhaylov <mmikhaylov56@gmail.com>
2020-07-03 19:01:41 +0300

    Polybar scipt: added '&'

--- .local/bin/launch_polybar.sh
+++ .local/bin/launch_polybar.sh
@@ -4,7 +4,7 @@ killall -q polybar
 
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload white
+    MONITOR=$m polybar --reload white &
   done
 else
   MONITOR=HDMI-1-1 polybar --reload white &
