15a7aa22e0f589505c415f5f1d1e82a7da7f4544
Maxim Mikhaylov <mmikhaylov56@gmail.com>
2020-07-03 18:51:10 +0300

    Added a script to launch polybar on all windows

---
+++ .local/bin/launch_polybar.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+killall -q polybar
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload white
+  done
+else
+  MONITOR=HDMI-1-1 polybar --reload white &
+fi
