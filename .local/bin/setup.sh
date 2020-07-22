4b26e9b3ee810c7791df5d70d6dd4d4f13aad880
Maxim Mikhaylov <mmikhaylov56@gmail.com>
2020-07-22 12:25:01 +0300

    setup: added vscode extension installation

--- .local/bin/setup.sh
+++ .local/bin/setup.sh
@@ -1,15 +1,31 @@
 #!/usr/bin/env sh
 
-if ! command -v yay &> /dev/null
-then
-    echo "Couldn't find `yay`"
-    if ! command -v pacman &> /dev/null
-    then
-        echo "Couldn't find `pacman` either, not arch-based distros aren't supported"
-        echo "You can install packages yourself with `<package manager> $(grep '^[a-Z]' ~/.config/pkglist)`"
-        exit 1
-    fi
-    echo "Trying to install `yay`"
-    sudo pacman -S yay || echo "`yay` installation failed, aborting..." && exit 1
-fi
-yay -S $(grep '^[a-Z]' ~/.config/pkglist)
+echo "Do you wish to install packages?"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) if ! command -v yay &> /dev/null
+              then
+                  echo "Couldn't find `yay`"
+                  if ! command -v pacman &> /dev/null
+                  then
+                      echo "Couldn't find `pacman` either, not arch-based distros aren't supported"
+                      echo "You can install packages yourself with `<package manager> $(grep '^[a-Z]' ~/.config/pkglist)`"
+                      exit 1
+                  fi
+                  echo "Trying to install `yay`"
+                  sudo pacman -S yay || echo "`yay` installation failed, aborting..." && exit 1
+              fi
+              yay -S $(grep '^[a-Z]' ~/.config/pkglist)
+              break;;
+        No ) break;;
+    esac
+done
+
+echo "Do you wish to install vscode extensions?"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) xargs -L 1 code --install-extension < ~/.config/Code\ -\ OSS/User/extensionlist
+              break;;
+        No ) break;;
+    esac
+done
