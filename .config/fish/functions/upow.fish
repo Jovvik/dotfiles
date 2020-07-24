4db69d1cdc427d3384c25b8dd8a141af10415aab
Maxim Mikhaylov <mmikhaylov56@gmail.com>
2020-07-24 16:25:30 +0300

    changed upow to a fish alias

---
+++ .config/fish/functions/upow.fish
@@ -0,0 +1,3 @@
+function upow --wraps='upower -i /org/freedesktop/UPower/devices/battery_BAT1' --description 'alias upow=upower -i /org/freedesktop/UPower/devices/battery_BAT1'
+    upower -i /org/freedesktop/UPower/devices/battery_BAT1
+end
