8c10d304af51d89cc61df3e9c86e64ec9a30fb5c
Maxim Mikhaylov <mmikhaylov56@gmail.com>
2020-07-30 16:02:23 +0300

    polybar: added separate numbers for aur/non-aur updates

--- .config/polybar/updates-arch-combined.sh
+++ .config/polybar/updates-arch-combined.sh
@@ -12,9 +12,9 @@ if ! updates_aur=$(yay -Qum 2> /dev/null | wc -l); then
     updates_aur=0
 fi
 
-updates=$(("$updates_arch" + "$updates_aur"))
+updates="$updates_arch/$updates_aur"
 
-if [ "$updates" -gt 0 ]; then
+if [ $((updates_arch + updates_aur)) -gt 0 ]; then
     echo "$updates"
 else
     echo ""
