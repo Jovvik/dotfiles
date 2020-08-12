dac0da538d2a92e3953ac3a80cc68c477a21b75a
Maxim Mikhaylov <mmikhaylov56@gmail.com>
2020-08-12 11:23:39 +0300

    polybar: minor additions to points.py, fixed formatting

--- .config/polybar/points.py
+++ .config/polybar/points.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 import pickle
-import os.path
+from os import path
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
@@ -11,15 +11,19 @@ SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
 SPREADSHEET_ID = '1GdlMd27JDscttvn7y8CNsYK5hSpU7GwebbD6khpE4os'
 RANGE = 'Лист1!B2:I3'
 
+
 def main():
     creds = None
-    if os.path.exists('/home/jovvik/.config/polybar/token.pickle'):
+    if path.exists('/home/jovvik/.config/polybar/token.pickle'):
         with open('/home/jovvik/.config/polybar/token.pickle', 'rb') as token:
             creds = pickle.load(token)
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
+            if not path.exists('credentials.json'):
+                print("Credentials missing, get them from google.")
+                return
             flow = InstalledAppFlow.from_client_secrets_file(
                 'credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
@@ -34,7 +38,9 @@ def main():
     if not values:
         print('!')
     else:
-        print('  '.join(f'{icon} {value}' for value, icon in zip(values, icons)))
+        print('  '.join(f'{icon} {value}' for value,
+                        icon in zip(values, icons)))
+
 
 if __name__ == '__main__':
     try:
