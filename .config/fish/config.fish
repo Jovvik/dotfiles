13f1315f0f658e9106dadd1c66507e10da0a8249
Maxim Mikhaylov <mmikhaylov56@gmail.com>
2020-08-29 13:10:48 +0300

    i3: added some shortcuts moved `dotcode` to a script for compatibility

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,3 @@
 alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
-function dotcode
-	GIT_WORK_TREE=$HOME GIT_DIR=$HOME/.dotfiles code $HOME
-end
+
 cat ~/.cache/wal/sequences &
