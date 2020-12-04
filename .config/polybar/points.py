20830746165159b7e43a8bf5cefde40da3c03037
Maxim Mikhaylov <mmikhaylov56@gmail.com>
2020-12-04 13:25:38 +0300

    completely reworked points.py now it doesn't rely on building a separate spreadsheet that collects info from other sheets instead the script calls each table separately this fixes

--- .config/polybar/points.py
+++ .config/polybar/points.py
@@ -11,6 +11,56 @@ SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
 SPREADSHEET_ID = '1GdlMd27JDscttvn7y8CNsYK5hSpU7GwebbD6khpE4os'
 RANGE = 'Лист1!B2:I3'
 
+SUBJS = [
+    {
+        "SPREADSHEET_ID": "1WyW_48ZFJFKsRIyG4ngizGZT-5XRcBS9RBkc048OZC4",
+        "RANGE": "Осень20!D48",
+        "ICON": "∫"
+    },
+    {
+        "SPREADSHEET_ID": "1XTorcnLrDwSc_Uq6WmjEvR6ncS8b-sG52rtAiKUcSRc",
+        "RANGE": "36-37!F45",
+        "ICON": ""
+    },
+    {
+        "SPREADSHEET_ID": "1AwMIB9hiiAB-0m6JSPdeTwYUga_nfynFcWWiD8lfAg8",
+        "RANGE": "F69:G69",
+        "ACTION": lambda x: sum(x),
+        "ICON": "↔"
+    },
+    {
+        "SPREADSHEET_ID": "1B52D85mRRVqPN5yymM0RyaxXLvgiy1xNeFfW-5OVdIM",
+        "RANGE": "B117",
+        "ICON": "dx"
+    },
+    {
+        "SPREADSHEET_ID": "1tlYlcoCE_yxRskQHu91t27V3S-vnsLkRUi9WAf1bAvs",
+        "RANGE": "BARS!D11:S11",
+        "ACTION": lambda x: sum(x),
+        "ICON": "en"
+    },
+    {
+        "SPREADSHEET_ID": "1xqnBXsoVrEV4NHiOo4JGlCZfgoy8yVPF8CIuzWM-w68",
+        "RANGE": "D120",
+        "ICON": "os"
+    },
+    {
+        "SPREADSHEET_ID": "1sBKYN5MJ5NyPRYsGaSUNRsxcAwsUJyZ-UxD4nbSnqGY",
+        "RANGE": "Баллы!F58",
+        "ACTION": lambda x: x[0] * 5,
+        "ICON": "pe"
+    }
+]
+
+
+def get_value(sheet, attrs):
+    result = sheet.values().get(spreadsheetId=attrs["SPREADSHEET_ID"],
+                                range=attrs["RANGE"]).execute().get('values', [])
+    result = [float(x.replace(',', '.')) for x in result[0]]
+    if "ACTION" in attrs:
+        return attrs["ACTION"](result)
+    return result[0]
+
 
 def main():
     creds = None
@@ -30,16 +80,10 @@ def main():
         with open('token.pickle', 'wb') as token:
             pickle.dump(creds, token)
 
-    service = build('sheets', 'v4', credentials=creds)
-    sheet = service.spreadsheets()
-    result = sheet.values().get(spreadsheetId=SPREADSHEET_ID, range=RANGE).execute()
-    values, icons = result.get('values', [])
-
-    if not values:
-        print('!')
-    else:
-        print('  '.join(f'{icon} {value}' for value,
-                        icon in zip(values, icons)))
+    sheet = build('sheets', 'v4', credentials=creds).spreadsheets()
+    results = [(subj["ICON"], get_value(sheet, subj)) for subj in SUBJS]
+    print('  '.join(' '.join((icon, str(value).rstrip('0').rstrip('.')))
+                    for icon, value in results))
 
 
 if __name__ == '__main__':
