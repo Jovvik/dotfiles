b312b282c2300fd5a38aa56deef0953c0702eda8
Maxim Mikhaylov <mmikhaylov56@gmail.com>
2020-12-18 12:16:44 +0300

    code: minor changes

---
+++ .config/cftemplate.cpp
@@ -0,0 +1,19 @@
+// ICIE v0.7.26
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll  = long long;
+using ull = unsigned long long;
+using v   = vector<ll>;
+using vv  = vector<v>;
+using vb  = vector<bool>;
+using p   = pair<ll, ll>;
+using vp  = vector<p>;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+}
