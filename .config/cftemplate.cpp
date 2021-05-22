eaa4dd770596e070e6679d1272c2d893014add2e
Maxim Mikhaylov <mmikhaylov56@gmail.com>
2021-05-22 19:53:00 +0300

    updated cf template

--- .config/cftemplate.cpp
+++ .config/cftemplate.cpp
@@ -1,5 +1,12 @@
 // ICIE v1.0.1
 
+#define _FORTIFY_SOURCE 0
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("no-stack-protector")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC target("sse,sse2,sse3,ssse3,popcnt,abm,mmx,tune=native,avx2,fma")
+#pragma GCC optimize("fast-math")
+
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -11,6 +18,9 @@ using vv  = vector<v>;
 using vb  = vector<bool>;
 using p   = pair<ll, ll>;
 using vp  = vector<p>;
+using s   = set<ll>;
+using c   = complex<long double>;
+using vc  = vector<c>;
 
 int main()
 {
